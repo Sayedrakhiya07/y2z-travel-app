3b94e70d8311cb784482eb7105c24b9011943cef
Sayedrakhiya07 <Sayedrakhiya07@gmail.com>
2025-06-05 15:44:26 +0530

    modify

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,29 +1,3 @@
 export const API_BASE_URL = "./assets/api/Itinerary-mock-data.json";
 
 
-const mapContainerStyle = {
-  width: "100%",
-  height: "100%",
-};
-
-const mapCenter = {
-  lat: 48.8606,
-  lng: 2.3376,
-};
-
-const mapMarkers = [
-  {
-    position: { lat: 48.8609, lng: 2.3365 },
-    label: "Le Bistrot des Halles",
-  },
-  {
-    position: { lat: 48.8603, lng: 2.3372 },
-    label: "Signature",
-  },
-  {
-    position: { lat: 48.8607, lng: 2.3369 },
-    label: "Cabinet d’Avocats Nakache Perez",
-  },
-];
-
-export { mapContainerStyle, mapCenter, mapMarkers };
